b3a2289a22f7bbfd17d392a8d3135a2be8019e31
Rishu Jain <77292616+codewithrishu@users.noreply.github.com>
2021-08-18 11:37:06 +0530

    final version
    final version of app

---
+++ work.js
@@ -0,0 +1,33 @@
+//storing html elements value
+var inputText = document.querySelector("#input-text");
+var outputText = document.querySelector("#output-text");
+var btn =  document.querySelector("#btn");
+
+   
+//adding input text to url
+function geturl(text) {
+    
+        return "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + text;
+    
+}
+
+//processing the url
+function fetchurl()
+ {
+     var input = inputText.value;
+    fetch(geturl(input))
+    .then(function (response) {
+        return response.json()
+        
+    })
+    .then(function (json) {
+        var translatedText = json.contents.translated;
+        outputText.innerText = translatedText
+        
+    })
+  
+}
+ 
+btn.addEventListener('click', fetchurl) 
+
+
